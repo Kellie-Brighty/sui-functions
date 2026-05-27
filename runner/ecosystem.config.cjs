52bb9c0d4a1b8b04ff8a056fc0927a8da1056621
Kelly <kelliebrighty@gmail.com>
2026-05-27 11:22:14 +0100

    Update ecosystem config to use node1.json, node2.json, node3.json

--- runner/ecosystem.config.cjs
+++ runner/ecosystem.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
         PACKAGE_ID: "0x8899b503f5f097546c61b698296ce44bc1f37251c3b7f3fa92d6e8036231dd30",
         PROTOCOL_TREASURY_ID: "0xa659782f25f332b3e947ef4557cd783ce182df87d0dcc71ad22069e7b8f0d789",
         REGISTRY_ID: "0x0",
-        OPERATOR_KEY_PATH: "/var/www/runner/keys/operator1.json"
+        OPERATOR_KEY_PATH: "/root/.sui-functions/node1.json"
       }
     },
     {
@@ -17,7 +17,7 @@ module.exports = {
         PACKAGE_ID: "0x8899b503f5f097546c61b698296ce44bc1f37251c3b7f3fa92d6e8036231dd30",
         PROTOCOL_TREASURY_ID: "0xa659782f25f332b3e947ef4557cd783ce182df87d0dcc71ad22069e7b8f0d789",
         REGISTRY_ID: "0x0",
-        OPERATOR_KEY_PATH: "/var/www/runner/keys/operator2.json"
+        OPERATOR_KEY_PATH: "/root/.sui-functions/node2.json"
       }
     },
     {
@@ -27,7 +27,7 @@ module.exports = {
         PACKAGE_ID: "0x8899b503f5f097546c61b698296ce44bc1f37251c3b7f3fa92d6e8036231dd30",
         PROTOCOL_TREASURY_ID: "0xa659782f25f332b3e947ef4557cd783ce182df87d0dcc71ad22069e7b8f0d789",
         REGISTRY_ID: "0x0",
-        OPERATOR_KEY_PATH: "/var/www/runner/keys/operator3.json"
+        OPERATOR_KEY_PATH: "/root/.sui-functions/node3.json"
       }
     }
   ]
