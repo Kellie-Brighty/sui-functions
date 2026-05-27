43ff7f262fa0d64202d1998c45cf94cafbcd27a4
Kelly <kelliebrighty@gmail.com>
2026-05-27 12:07:43 +0100

    Update ecosystem.config.cjs with correct package/treasury/registry IDs and interpreter path

--- runner/ecosystem.config.cjs
+++ runner/ecosystem.config.cjs
@@ -3,30 +3,33 @@ module.exports = {
     {
       name: "sui-operator-1",
       script: "dist/listener.js",
+      interpreter: "/usr/bin/node",
       env: {
-        PACKAGE_ID: "0x8899b503f5f097546c61b698296ce44bc1f37251c3b7f3fa92d6e8036231dd30",
-        PROTOCOL_TREASURY_ID: "0xa659782f25f332b3e947ef4557cd783ce182df87d0dcc71ad22069e7b8f0d789",
-        REGISTRY_ID: "0x0",
+        PACKAGE_ID: "0x24efc151bdf08afcdbe857e34b5bbc5cebf03feef2b076fa4904af481e2e6bb1",
+        PROTOCOL_TREASURY_ID: "0x24d4f1ab704659bcc694e8667bec4af44725db6d262ab8742bb2df237189e3fb",
+        REGISTRY_ID: "0xeb24efabffbc663e1e59c9e1420a0424a4142a12db78138ce1e05ceb51f83c9f",
         OPERATOR_KEY_PATH: "/root/.sui-functions/node1.json"
       }
     },
     {
       name: "sui-operator-2",
       script: "dist/listener.js",
+      interpreter: "/usr/bin/node",
       env: {
-        PACKAGE_ID: "0x8899b503f5f097546c61b698296ce44bc1f37251c3b7f3fa92d6e8036231dd30",
-        PROTOCOL_TREASURY_ID: "0xa659782f25f332b3e947ef4557cd783ce182df87d0dcc71ad22069e7b8f0d789",
-        REGISTRY_ID: "0x0",
+        PACKAGE_ID: "0x24efc151bdf08afcdbe857e34b5bbc5cebf03feef2b076fa4904af481e2e6bb1",
+        PROTOCOL_TREASURY_ID: "0x24d4f1ab704659bcc694e8667bec4af44725db6d262ab8742bb2df237189e3fb",
+        REGISTRY_ID: "0xeb24efabffbc663e1e59c9e1420a0424a4142a12db78138ce1e05ceb51f83c9f",
         OPERATOR_KEY_PATH: "/root/.sui-functions/node2.json"
       }
     },
     {
       name: "sui-operator-3",
       script: "dist/listener.js",
+      interpreter: "/usr/bin/node",
       env: {
-        PACKAGE_ID: "0x8899b503f5f097546c61b698296ce44bc1f37251c3b7f3fa92d6e8036231dd30",
-        PROTOCOL_TREASURY_ID: "0xa659782f25f332b3e947ef4557cd783ce182df87d0dcc71ad22069e7b8f0d789",
-        REGISTRY_ID: "0x0",
+        PACKAGE_ID: "0x24efc151bdf08afcdbe857e34b5bbc5cebf03feef2b076fa4904af481e2e6bb1",
+        PROTOCOL_TREASURY_ID: "0x24d4f1ab704659bcc694e8667bec4af44725db6d262ab8742bb2df237189e3fb",
+        REGISTRY_ID: "0xeb24efabffbc663e1e59c9e1420a0424a4142a12db78138ce1e05ceb51f83c9f",
         OPERATOR_KEY_PATH: "/root/.sui-functions/node3.json"
       }
     }
